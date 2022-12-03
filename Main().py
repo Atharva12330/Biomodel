4268c6e01a2c1f6d134eaea44dd4d1ebfcc6832b
Atharva12330 <119721701+Atharva12330@users.noreply.github.com>
2022-12-03 11:12:02 +0530

    main files
    base.py

---
+++ Main().py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  3 10:55:50 2022
+
+@author: hp
+"""
+symp = input("Enter symtpoms : ")
+
+CAD = ['Chest pain', 'body pain', 'falling sick', 'faint']
+
+if symp in CAD:
+    
